a40c2aed1fe52898159c37cb6b4be307250d0b0a
rewad9810 <35177157+rewad9810@users.noreply.github.com>
2018-01-27 13:47:39 +0400

    Update minimap.user.js

--- minimap.user.js
+++ minimap.user.js
@@ -1,23 +1,23 @@
 // ==UserScript==
-// @name         PixelTr Minimap
-// @namespace    http://discord.io/pixeltr
+// @name         strawberry Minimap
+// @namespace    strawberry
 // @version      3.1.1
-// @description  Pixelcanvas Minimap     ☪ | PixelTurkey | ☪
+// @description  strawberry
 // @author       Some Anon && Qh0stM4N
 // @match        https://pixelcanvas.io/*
 // @match        http://pixelcanvas.io/*
-// @homepage     https://github.com/Priz001/Pixelcanvas-Minimap/
-// @updateURL    https://raw.githubusercontent.com/Priz001/Pixelcanvas-Minimap/master/minimap.user.js
-// @downloadURL  https://raw.githubusercontent.com/Priz001/Pixelcanvas-Minimap/master/minimap.user.js
+// @homepage     https://github.com/rewad9810/strawberry/
+// @updateURL    https://raw.githubusercontent.com/rewad9810/strawberry/master/minimap.user.js
+// @downloadURL  https://raw.githubusercontent.com/rewad9810/strawberry/master/minimap.user.js
 // @grant        none
 // ==/UserScript==
 
 //ref:https://stackoverflow.com/questions/4604663/download-single-files-from-github
 //github raw file url  template
 //https://raw.githubusercontent.com/user/repository/branch/filename
-//https://raw.githubusercontent.com/Priz001/Pixelcanvas-Minimap/master/README.md
+//https://raw.githubusercontent.com/rewad9810/strawberry/master/README.md
 
-window.baseTepmlateUrl = 'https://raw.githubusercontent.com/Priz001/Pixelcanvas-Minimap/master';
+window.baseTepmlateUrl = 'https://raw.githubusercontent.com/rewad9810/strawberry/master';
 
 window.addEventListener('load', function () {
     //Regular Expression to get coordinates out of URL
